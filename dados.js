b3ff7b05ab7f36227adb3f272d300f42dfcd4df8
filipinmusica-hue <filipinmusica@gmail.com>
2026-04-25 15:10:56 -0300

    Atualizar o dados.js

--- dados.js
+++ dados.js
@@ -6,27 +6,30 @@ const projetos = [
     ouvir: "https://youtube.com",
     musicas: [
       { nome: "Música 1" },
-      { nome: "Música 2" },
-      { nome: "Música 3" }
+      { nome: "Música 2" }
     ]
   },
+
   {
     nome: "EP Noturno",
     tipo: "ep",
     capa: "ep1.jpg",
     ouvir: "https://youtube.com",
     musicas: [
-      { nome: "Música 4" },
-      { nome: "Música 5" }
+      { nome: "Música 3" },
+      { nome: "Música 4" }
     ]
   },
+
   {
-    nome: "Single 1",
+    nome: "Novo Lançamento",
     tipo: "single",
-    capa: "single1.jpg",
+    capa: "lancamento.jpg",
+    data: "2026-05-10T00:00:00", // DATA DO LANÇAMENTO
+    preSave: "https://youtube.com",
     ouvir: "https://youtube.com",
     musicas: [
-      { nome: "Single 1" }
+      { nome: "Nova Música" }
     ]
   }
 ];
