ccfebead93f6eaf95c522486f872de54fd29e15c
filipinmusica-hue <filipinmusica@gmail.com>
2026-04-25 11:27:04 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,15 +1,28 @@
 const musicas = [
   {
-    nome: "baby",
+    nome: "Nova Música",
     capa: "capa1.jpg",
     data: "2026-05-01T00:00:00",
-    preSave: "https://link-presave.com",
+    plays: 1500,
+    preSave: "https://SEU-PRESAVE.com",
     ouvir: "https://spotify.com"
   },
   {
-    nome: "Minha Música 2",
+    nome: "Música Antiga 1",
     capa: "capa2.jpg",
-    data: "2026-04-01T00:00:00",
+    plays: 900,
+    ouvir: "https://spotify.com"
+  },
+  {
+    nome: "Música Antiga 2",
+    capa: "capa3.jpg",
+    plays: 600,
+    ouvir: "https://spotify.com"
+  },
+  {
+    nome: "Música Antiga 3",
+    capa: "capa4.jpg",
+    plays: 300,
     ouvir: "https://spotify.com"
   }
 ];
