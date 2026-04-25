20aa4343946d3afb61961fcaf0db7a4294f1a652
filipinmusica-hue <filipinmusica@gmail.com>
2026-04-25 02:28:18 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,15 @@
+const musicas = [
+  {
+    nome: "Minha Música 1",
+    capa: "capa1.jpg",
+    data: "2026-05-01T00:00:00",
+    preSave: "https://link-presave.com",
+    ouvir: "https://spotify.com"
+  },
+  {
+    nome: "Minha Música 2",
+    capa: "capa2.jpg",
+    data: "2026-04-01T00:00:00",
+    ouvir: "https://spotify.com"
+  }
+];
