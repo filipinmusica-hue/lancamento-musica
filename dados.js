3f095c63c9bbe1d7a2d8fe9164676e8a8fd01fd5
filipinmusica-hue <filipinmusica@gmail.com>
2026-04-26 19:20:54 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -38,7 +38,7 @@ window.projetos = [
       {
     nome: "VVS",
     tipo: "SINGLE",
-    capa: "vss.jpg",
+    capa: "vvs.jpg",
     ouvir: "https://youtu.be/lSjzSC5Qy3Y?si=v8aOFKag_jMue7Js",
     musicas: [
       { nome: "VVS" }
@@ -51,6 +51,114 @@ window.projetos = [
     ouvir: "https://youtu.be/nhISltprs_U?si=Ny6y62iFq5_v8jFl",
     musicas: [
       { nome: "Number One" }
+    ]
+  },
+   {
+    nome: "Vida de chefe",
+    tipo: "SINGLE",
+    capa: "vida_de_chefe.jpg",
+    ouvir: "https://youtu.be/L2FQ_J-ZiMQ?si=3p0725ooeMBdsxoc",
+    musicas: [
+      { nome: "Vida de chefe" }
+    ]
+  },
+     {
+    nome: "Minha Fã",
+    tipo: "SINGLE",
+    capa: "minha fa.jpg",
+    ouvir: "https://youtu.be/cf-ygCH8oxk?si=QlW9QELWT0E6jc82",
+    musicas: [
+      { nome: "Minha Fã" }
+    ]
+  },
+   {
+    nome: "048",
+    tipo: "SINGLE",
+    capa: "048.jpg",
+    ouvir: "https://youtu.be/6KYg-L8BpG0?si=PofUyQ9zRHrENflu",
+    musicas: [
+      { nome: "048" }
+    ]
+  },
+   {
+    nome: "Baby 2",
+    tipo: "SINGLE",
+    capa: "baby_2.jpg",
+    ouvir: "https://youtu.be/tMQXyXUwWYk?si=_zHRyZcpR0rPh71-",
+    musicas: [
+      { nome: "Baby 2" }
+    ]
+  },
+  {
+    nome: "Bad Girl",
+    tipo: "SINGLE",
+    capa: "bad_girl.jpg",
+    ouvir: "https://youtu.be/y8-x0-R76pw?si=7IMN2Ok4tr8qaGb3",
+    musicas: [
+      { nome: "Bad Girl" }
+    ]
+  },
+   {
+    nome: "New Wave",
+    tipo: "ALBUM",
+    capa: "new_wave.jpg",
+    ouvir: "https://youtube.com/playlist?list=PL-pksmpo7d-0hUtzS4MKSVTq3PrqyTrS8&si=AygqyG0EDD5JbcLc",
+    musicas: [
+      { nome: "New Wave" }
+    ]
+  },
+  {
+    nome: "F*ck Lov&",
+    tipo: "SINGLE",
+    capa: "fuck love.jpg",
+    ouvir: "https://youtu.be/wtjysuEOotE?si=yPdCSmPD62EHp6lW",
+    musicas: [
+      { nome: "F*ck Lov&" }
+    ]
+  },
+  {
+    nome: "Noite Escura",
+    tipo: "ALBUM",
+    capa: "noite escura.jpg",
+    ouvir: "https://youtube.com/playlist?list=PL-pksmpo7d-0pQoZ55HPM_Ge4R-TCsfGx&si=pKFlOjypnnkHH3rk",
+    musicas: [
+      { nome: "Noite Escura" }
+    ]
+  },
+  {
+    nome: "Drugs",
+    tipo: "SINGLE",
+    capa: "drogs.jpg",
+    ouvir: "https://youtu.be/t8_uUHCE56M?si=_C0rKfeB3XrCWJjk",
+    musicas: [
+      { nome: "Drugs" }
+    ]
+  },
+  {
+    nome: "Virada.WAV",
+    tipo: "ALBUM",
+    capa: "virada.jpg",
+    ouvir: "https://youtube.com/playlist?list=PL-pksmpo7d-0pQoZ55HPM_Ge4R-TCsfGx&si=pKFlOjypnnkHH3rk",
+    musicas: [
+      { nome: "Virada.WAV" }
+    ]
+  },
+  {
+    nome: "Sem Medo",
+    tipo: "SINGLE",
+    capa: "sem medo.jpg",
+    ouvir: "https://youtu.be/PkIV469uN6Y?si=uwVOBNbuI544TFoO",
+    musicas: [
+      { nome: "Sem Medo" }
+    ]
+  },
+   {
+    nome: "Brilho No Olhar",
+    tipo: "SINGLE",
+    capa: "brilho no olhar.jpg",
+    ouvir: "https://youtu.be/6tKZkiIM518?si=1qvrvGSVESHc-u2d",
+    musicas: [
+      { nome: "Brilho No Olhar" }
     ]
   },
   {
